3f86683d7cb83d41c362690d72ab746bdd353bc0
Willy <Willian2006.com@gmail.com>
2025-03-24 09:11:30 -0300

    IMC!!!!!
    IMC IMC IMC IMC

--- app/src/main/java/com/ifsc/contaclique/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclique/MainActivity.java
@@ -3,6 +3,7 @@ package com.ifsc.contaclique;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,28 +12,30 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     int i=0;
+    EditText edpeso,edaltura;
+    TextView tvresultado;
+    Button buttonCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.textView);
-        tv.setText(getString(R.string.app_name));
+        edpeso=findViewById(R.id.edpeso);
+        edaltura=findViewById(R.id.edaltura);
+        tvresultado=findViewById(R.id.tvresultadoimc);
+        buttonCalcular=findViewById(R.id.button);
+        //define um tratamento do botão
+        buttonCalcular.setOnClickListener(v->{
+            //calcular imc
+            //recuperar os dados de peso e altura
+            double peso,altura,imc;
+            peso = Double.parseDouble(edpeso.getText().toString());
+            altura = Double.parseDouble(edaltura.getText().toString());
+            imc = peso/altura;
 
-        Button b = findViewById(R.id.button);
-
-        b.setOnClickListener (v -> {//SUA LAMBIDA AQUI!!!
+            tvresultado.setText(Double.toString(imc));
         });
 
-        b.setOnClickListener(v -> {});
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tv.setText(Integer.toString(i));
-                i++;
-            }
-        });
     }
 }
