130397dfaaa269376cbd9f8ebadcaee482bfe22f
Willy14 <Willian2006.com@gmail.com>
2025-05-12 08:36:06 -0300

    Classe planeta

---
+++ app/src/main/java/com/ifsc/contaclique/Planeta.java
@@ -0,0 +1,15 @@
+package com.ifsc.contaclique;
+
+import android.graphics.Bitmap;
+
+public class Planeta {
+    String nome;
+    //Bitmap imagem;
+    Integer imagme; //são recursos da pasta drawable R.drawable.nomeDaImagem
+
+    public Planeta(String nome, Integer imagme) {
+        this.nome = nome;
+        this.imagme = imagme;
+    }
+}
+
