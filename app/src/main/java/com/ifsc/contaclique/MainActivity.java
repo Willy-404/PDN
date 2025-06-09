2b8ce6656a15ea2a04df17f29802f0f4e363e3cd
WIlly <willian2006.com@gmail.com>
2025-06-09 08:59:17 -0300

    SENSOREANDO MUITAS COISAS

--- app/src/main/java/com/ifsc/contaclique/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclique/MainActivity.java
@@ -6,8 +6,6 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -18,27 +16,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     int i=0;
     SensorManager mSensorManager;
     Sensor sensor;
-    TextView tv;
+    TextView tx, ty, tz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.textView);
+        tx = findViewById(R.id.textViewX);
+        ty = findViewById(R.id.textViewY);
+        tz = findViewById(R.id.textViewZ);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         //metodo para verificar sensores
         //mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
 
-        sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         mSensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        tv.setText(Float.toString(sensorEvent.values[0]));
+        tx.setText(Float.toString(sensorEvent.values[0]));
+        ty.setText(Float.toString(sensorEvent.values[1]));
+        tz.setText(Float.toString(sensorEvent.values[2]));
     }
 
     @Override
