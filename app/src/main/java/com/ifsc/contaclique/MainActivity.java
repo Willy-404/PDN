a45a1c74570e1d8188fc9d772dc4a77db709c576
Willy14 <willian2006.com@gmail.com>
2025-06-30 09:07:18 -0300

    RASTREANDO TUDO

--- app/src/main/java/com/ifsc/contaclique/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclique/MainActivity.java
@@ -1,5 +1,10 @@
 package com.ifsc.contaclique;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,32 +12,60 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    int i=0;
+    LocationManager locationManager;
+    TextView tvLatitude, tvLongitude, tvStatus;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        tvLatitude=findViewById(R.id.tvLatitude);
+        tvLongitude=findViewById(R.id.tvLongitude);
+        tvStatus=findViewById(R.id.tvStatus);
 
-        TextView tv = findViewById(R.id.tvLatitude);
-        tv.setText(getString(R.string.app_name));
+        button = findViewById(R.id.button);
 
-        Button b = findViewById(R.id.button);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        b.setOnClickListener (v -> {//SUA LAMBIDA AQUI!!!
+        button.setOnClickListener(view -> {
+            getLocalizacao();
         });
 
-        b.setOnClickListener(v -> {});
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tv.setText(Integer.toString(i));
-                i++;
+        public void getLocalizacao(){
+            if (checaSolicitaPermicao()){
+                Location l = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                tvLatitude.setText(Double.toString(l.getLatitude()));
+                tvLongitude.setText(Double.toString(l.getLongitude()));
+            }else{
+                tvLatitude.setText("Permissão Negada");
             }
-        });
+        }
+
+        public boolean checaSolicitaPermicao(){
+            //verifica se tem e caso não, pede permissão
+            if(ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+            ||
+            ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+            )
+            {//codego
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                                },
+                    PackageManager.PERMISSION_GRANTED
+                    );
+            return false;
+            }else{
+            return true;
+            }
+        }
     }
 }
